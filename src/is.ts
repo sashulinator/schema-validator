87ab9e53e5f86fc39bf495670622f71ad2c0ecff
savchenko alexander <ext.adsavchenko@tinkoff.ru>
2022-01-18 21:13:02 +0400

    add some "is"

--- src/is.ts
+++ src/is.ts
@@ -1,21 +1,25 @@
-export function isNotEmptyString<T>(value: T | ''): value is T {
-  return value !== ''
+export function isNil(input: unknown): input is undefined | null {
+  return input === null && input === undefined
 }
 
-export function isNotNil<T>(value: T | undefined | null): value is T {
-  return value !== null && value !== undefined
+export function isNotNil<T>(input: T | undefined | null): input is T {
+  return input !== null && input !== undefined
 }
 
-export function isNotNull<T>(value: T | null): value is T {
-  return value !== null
+export function isNull(input: unknown): input is null {
+  return input === null
 }
 
-export function isNotUndefined<T>(value: T | undefined): value is T {
-  return value !== undefined
+export function isNotNull<T>(input: T | null): input is T {
+  return input !== null
 }
 
-export function isNotZero<T>(value: T | 0): value is T {
-  return value !== 0
+export function isUndefined(input: unknown): input is unknown {
+  return input === undefined
+}
+
+export function isNotUndefined<T>(input: T | undefined): input is T {
+  return input !== undefined
 }
 
 export function isNumber(input: unknown): input is number {
@@ -25,6 +29,18 @@ export function isNumber(input: unknown): input is number {
   return false
 }
 
+export function isNotNumber<T>(input: T | number): input is T {
+  return !isNumber(input)
+}
+
+export function isZero<T>(input: T | 0): input is T {
+  return input === 0
+}
+
+export function isNotZero<T>(input: T | 0): input is T {
+  return input !== 0
+}
+
 export function isString(input: unknown): input is string {
   if (typeof input === 'string' || input instanceof String) {
     return true
@@ -32,6 +48,22 @@ export function isString(input: unknown): input is string {
   return false
 }
 
+export function isNotString<T>(input: string | T): input is T {
+  return !isString(input)
+}
+
+export function isEmptyString<T>(input: T | ''): input is '' {
+  return input === ''
+}
+
+export function isNotEmptyString<T>(input: T | ''): input is T {
+  return input !== ''
+}
+
 export function isBoolean(input: unknown): input is boolean {
   return input === true || input === false
+}
+
+export function isNotBoolean<T>(input: boolean | T): input is T {
+  return !isBoolean(input)
 }
