f65b5c9a8864147274ce8bcd432de01daff67c16
savchenko alexander <ext.adsavchenko@tinkoff.ru>
2022-01-19 00:39:02 +0400

    add witValue

--- src/with-ref.ts
+++ src/with-ref.ts
@@ -1,14 +1,24 @@
 import { ComparingAssertion, ObjectStructure } from './types'
 
-export type CompareWith = (
+export type WithRef = (
   keyInObjectStructure: string,
   comparingAssertion: ComparingAssertion,
   name?: string,
 ) => [ComparingAssertion, any | ((value: any, key?: string, structure?: ObjectStructure) => any), string?]
 
-export const withRef: CompareWith = (keyInObjectStructure, comparingAssertion, name) => {
+export const withRef: WithRef = (keyInObjectStructure, comparingAssertion, name) => {
   function getValueFromObjectStructure(value: any, key?: string, objStructure?: any): any {
     return objStructure[keyInObjectStructure]
   }
   return [comparingAssertion, getValueFromObjectStructure, name ?? keyInObjectStructure]
+}
+
+export type WithValue = (
+  value2: any,
+  comparingAssertion: ComparingAssertion,
+  key2?: string,
+) => [ComparingAssertion, any | ((value: any, key?: string, structure?: ObjectStructure) => any), string?]
+
+export const withValue: WithValue = (value2, comparingAssertion, key2) => {
+  return [comparingAssertion, value2, key2]
 }
