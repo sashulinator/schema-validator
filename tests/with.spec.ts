bf4f228a7e33e29cd5fc5fb24175071153f316bc
Savchenko Alexander <sashulinator@gmail.com>
2022-02-17 22:16:45 +0400

    test: withValue

--- tests/with.spec.ts
+++ tests/with.spec.ts
@@ -1,10 +1,14 @@
-import { assertMatchPattern, assertRegExp, assertString, withRef } from '../src'
+import { assertMatchPattern, assertRegExp, assertString, withRef, withValue } from '../src'
 import { ssv2 } from './schemas'
 
 describe('with', () => {
   const validateTest = ssv2.wrap({
     pattern: assertRegExp,
     test: withRef('pattern', assertMatchPattern),
+  })
+
+  const validateTest2 = ssv2.wrap({
+    test: withValue(/^test/, assertMatchPattern),
   })
 
   it('withRef basic', () => {
@@ -20,6 +24,22 @@ describe('with', () => {
         _input2: '/^test/',
         _inputName: 'test',
         _inputName2: 'pattern',
+        _message: 'does not match the pattern',
+      },
+    })
+  })
+
+  it('withValue basic', () => {
+    const errors = validateTest2({
+      test: 'nottest',
+    })
+
+    expect(errors).toStrictEqual({
+      test: {
+        _code: 'assertMatchPattern',
+        _input: 'nottest',
+        _input2: '/^test/',
+        _inputName: 'test',
         _message: 'does not match the pattern',
       },
     })
