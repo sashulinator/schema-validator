48fea767e77f6ba90a5c3e3323bf8f063a72dce1
Savchenko Alexander <sashulinator@gmail.com>
2022-04-18 16:09:48 +0400

    remove trash code

--- src/helpers.ts
+++ src/helpers.ts
@@ -21,14 +21,3 @@ export function createStructureValidator<TErrors>(
     return emitStructureValidator as TSchema & EmitStructureValidation<TErrors>
   }
 }
-
-export function addPropertiesToFunction<
-  Obj extends Record<string, unknown>,
-  Func extends (...args: unknown[]) => unknown
->(obj: Obj, fn: Func): Func & Obj {
-  Object.entries(obj).forEach(([key, value]) => {
-    Object.defineProperty(fn, key, { value, writable: true, enumerable: true })
-  })
-
-  return fn as Func & Obj
-}
